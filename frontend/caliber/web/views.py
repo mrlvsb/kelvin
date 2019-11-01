22a97726b8d6b5ea7270e24cf8438c4335e0ab8d
Jan Gaura <jan.gaura@gmail.com>
2019-11-01 17:04:17 +0100

    Separate urls + index page

--- frontend/caliber/web/views.py
+++ frontend/caliber/web/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the caliber index.")
