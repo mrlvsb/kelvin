3c48470598e7f3f75d48f3423624852516d098e1
Jan Gaura <jan.gaura@gmail.com>
2019-11-01 14:22:48 +0100

    Submission script stub

---
+++ scripts/submit.py
@@ -0,0 +1,7 @@
+"""
+This script sends a source code to the submission server.
+"""
+
+# TODO:
+# - Sending source code to server as one file
+# - Sending more files as a batch (possibly a zip file?)
