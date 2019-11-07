c39c1c9da95218eeef3cd681c678c645dce03f6a
Jan Gaura <jan.gaura@gmail.com>
2019-11-07 09:28:06 +0100

    Fixed error in urls defs

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ll', views.ll, name='ll'),
     path('task/<int:id>', views.task_detail, name='task_detail'),
     path('task/<int:id>/<int:submit_id>', views.task_detail, name='task_detail'),
     path('install_<str:token>.sh', views.script, name='install.sh'),
