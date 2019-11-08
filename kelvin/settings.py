9886a28073819c1b008a45b755cf976d976bf22f
Daniel Trnka <daniel.trnka@gmail.com>
2019-11-08 14:14:04 +0100

    settings: added local domains

--- kelvin/settings.py
+++ kelvin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['upr.cs.vsb.cz']
+ALLOWED_HOSTS = ['upr.cs.vsb.cz', 'localhost', '127.0.0.1']
 
 
 # Application definition
