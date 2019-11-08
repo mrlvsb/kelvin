82bcecd03e961a057397c8a979324cbf8c1cd60d
Daniel Trnka <daniel.trnka@gmail.com>
2019-11-08 14:50:57 +0100

    api submit: evaluate points

--- api/views.py
+++ api/views.py
@@ -19,6 +19,16 @@ def post(s: Submit):
     )
 
     s.result = json.dumps(result, indent=4)
+
+    # calculate points
+    s.points = 0
+    s.max_points = 0
+    for i in result:
+        for test in i['tests']:
+            if test['success']:
+                s.points += 1
+            s.max_points += 1
+
     s.save()
 
 @csrf_exempt
