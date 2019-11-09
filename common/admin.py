be2ab068f7997bbc74a9358ef8d7730cdfe8dc97
Daniel Trnka <daniel.trnka@gmail.com>
2019-11-09 13:03:00 +0100

    class admin: show only teachers, select2 for students

--- common/admin.py
+++ common/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 import common.models as models
+from django.contrib.auth.models import User
+
+
+class ClassAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['students']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "teacher":
+            kwargs["queryset"] = User.objects.filter(groups__name='teacher')
+        print(db_field.name)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 
 admin.site.register(models.Task)
-admin.site.register(models.Class)
+admin.site.register(models.Class, ClassAdmin)
 admin.site.register(models.Submit)
 admin.site.register(models.AssignedTask)
