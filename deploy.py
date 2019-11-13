33ffdb4a897d4a3c00b6860439b1fd110f0ba20a
Daniel Trnka <daniel.trnka@gmail.com>
2019-11-13 12:45:08 +0100

    Adeded deploy script

---
+++ deploy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from fabric import task, Connection
+
+c = Connection("kelvin@kelvin")
+with c.cd("kelvin"):
+    c.run("git stash")
+    c.run("git pull")
+    c.run("git stash pop")
+    c.run("~/venv/bin/python manage.py migrate")
+    c.run("~/venv/bin/python manage.py collectstatic --no-input")
+
+c.sudo("systemctl restart uwsgi kelvin-worker@0")
+
