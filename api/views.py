aa2fad80dd20984d0b6b4e3d0873aa0a92565aa8
Daniel Trnka <daniel.trnka@gmail.com>
2019-11-14 05:03:41 +0100

    api: return errors

--- api/views.py
+++ api/views.py
@@ -15,20 +15,23 @@ from common.evaluate import evaluate_job
 def submit(request, task_code):
     auth_header = request.headers.get('Authorization')
     if not auth_header:
-        return HttpResponse(status=400)
+        return HttpResponse('Missing Authorization header', status=400)
 
-    token = auth_header.split(' ')[-1]
-    found_token = UserToken.objects.filter(token=token).first()
-    if not found_token:
-        return HttpResponse(status=401)
+    try:
+        token = auth_header.split(' ')[-1]
+        found_token = UserToken.objects.get(token=token)
+        
+        s = Submit()
+        s.source = request.FILES['solution']
+        s.student = found_token.user
+        s.assignment = AssignedTask.objects.get(task__code=task_code, clazz__students__id=found_token.user.id)
+        s.submit_num = Submit.objects.filter(assignment__id=s.assignment.id, student__id=found_token.user.id).count() + 1
+        s.save()
+
+        django_rq.enqueue(evaluate_job, s)
     
-    s = Submit()
-    s.source = request.FILES['solution']
-    s.student = found_token.user
-    s.assignment = AssignedTask.objects.get(task__code=task_code, clazz__students__id=found_token.user.id)
-    s.submit_num = Submit.objects.filter(assignment__id=s.assignment.id, student__id=found_token.user.id).count() + 1
-    s.save()
-
-    django_rq.enqueue(evaluate_job, s)
-   
-    return HttpResponse(request.build_absolute_uri(reverse('task_detail', kwargs={'assignment_id': s.assignment.id})))
+        return HttpResponse(request.build_absolute_uri(reverse('task_detail', kwargs={'assignment_id': s.assignment.id})))
+    except UserToken.DoesNotExist:
+        return HttpResponse('Authorization token not found', status=401)
+    except AssignedTask.DoesNotExist:
+        return HttpResponse('Task does not exist', status=404)
