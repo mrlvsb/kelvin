bb75caa4948a945a384aee930d97704e56ab9145
Daniel Trnka <daniel.trnka@gmail.com>
2020-03-15 13:06:11 +0100

    backup: backup tasks also

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ dst="backups/$(date +"%Y-%m-%d_%H:%M:%S")"
 mkdir -p "$dst"
 cd "$dst"
 
-rsync -avz kelvin@kelvin.cs.vsb.cz:kelvin/{db.sqlite3,submits} .
+rsync -avz kelvin@kelvin.cs.vsb.cz:kelvin/{db.sqlite3,submits,tasks} .
