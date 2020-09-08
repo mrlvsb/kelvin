21a3bfc1ab0b9ef5a723e3f53afde7dce4d38e08
Daniel Trnka <daniel.trnka@gmail.com>
2020-09-08 23:53:44 +0200

    deploy: rebuild docker images

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,6 @@ ssh kelvin@kelvin.cs.vsb.cz << COMMANDS
   python manage.py migrate
   python manage.py collectstatic --no-input -c
   sudo systemctl restart uwsgi kelvin-worker@0
+  cd evaluator/images && ./build.sh
   echo OK
 COMMANDS
