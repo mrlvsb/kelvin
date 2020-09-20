18e4910894fb39579991eeb933fc32a2985cde9c
Jakub Beránek <jakub.beranek@vsb.cz>
2020-09-20 15:28:11 +0200

    Fix MOSS job timeout and result check

--- common/moss.py
+++ common/moss.py
@@ -13,39 +13,40 @@ from common.models import Submit
 
 @django_rq.job
 def check_task(task_id):
-    submits = Submit.objects.filter(assignment__task__id=task_id).order_by('-submit_num')
-    m = mosspy.Moss(settings.MOSS_USERID, "c")
+    try:
+        submits = Submit.objects.filter(assignment__task__id=task_id).order_by('-submit_num')
+        m = mosspy.Moss(settings.MOSS_USERID, "c")
 
-    # template files
-    tpl_path = os.path.join(submits[0].assignment.task.dir(), "template")
-    for root, _, files in os.walk(tpl_path):
-        for f in files:
-            m.addBaseFile(os.path.join(root, f))
+        # template files
+        tpl_path = os.path.join(submits[0].assignment.task.dir(), "template")
+        for root, _, files in os.walk(tpl_path):
+            for f in files:
+                m.addBaseFile(os.path.join(root, f))
 
-    processed = set()
-    for submit in submits:
-        if submit.student_id not in processed:
-            processed.add(submit.student_id)
-            for source in submit.all_sources():
-                m.addFile(source.phys, os.path.join(submit.student.username, source.virt))
+        processed = set()
+        for submit in submits:
+            if submit.student_id not in processed:
+                processed.add(submit.student_id)
+                for source in submit.all_sources():
+                    m.addFile(source.phys, os.path.join(submit.student.username, source.virt))
 
-    url = m.send()
-    with tempfile.NamedTemporaryFile() as out:
-        m.saveWebPage(url, out.name)
+        url = m.send()
+        with tempfile.NamedTemporaryFile() as out:
+            m.saveWebPage(url, out.name)
 
-        matches = []
-        with open(out.name) as f:
-            regex = r'<TR>' \
-                '<TD><A HREF="(?P<link>[^"]+)">(?P<first_login>[^/]+)/(?P<first_path>.*?) \((?P<first_percent>\d+)%\)</A>' \
-                '\s*<TD><A HREF="[^"]+">(?P<second_login>[^/]+)/(?P<second_path>.*?) \((?P<second_percent>\d+)%\)</A>' \
-                '\s*<TD[^>]+>(?P<lines>\d+)'
+            matches = []
+            with open(out.name) as f:
+                regex = r'<TR>' \
+                    '<TD><A HREF="(?P<link>[^"]+)">(?P<first_login>[^/]+)/(?P<first_path>.*?) \((?P<first_percent>\d+)%\)</A>' \
+                    '\s*<TD><A HREF="[^"]+">(?P<second_login>[^/]+)/(?P<second_path>.*?) \((?P<second_percent>\d+)%\)</A>' \
+                    '\s*<TD[^>]+>(?P<lines>\d+)'
 
-            for m in re.finditer(regex, f.read()):
-                d = m.groupdict()
-                d['first_percent'] = int(d['first_percent'])
-                d['second_percent'] = int(d['second_percent'])
-                matches.append(d)
+                for m in re.finditer(regex, f.read()):
+                    d = m.groupdict()
+                    d['first_percent'] = int(d['first_percent'])
+                    d['second_percent'] = int(d['second_percent'])
+                    matches.append(d)
 
-            caches['default'].set(f'moss.{task_id}', matches, timeout=60*60*24*10)
-            caches['default'].delete(f'moss.job.{task_id}')
-
+                caches['default'].set(f'moss.{task_id}', matches, timeout=60*60*24*10)
+    finally:
+        caches['default'].delete(f'moss.job.{task_id}')
