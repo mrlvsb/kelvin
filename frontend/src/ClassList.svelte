8ad92c61fdaab1075d3c338f0a5ea6ed3790fef3
Daniel Trnka <daniel.trnka@gmail.com>
2020-10-15 22:01:28 +0200

    ClassList: allow to filter subjects

--- frontend/src/ClassList.svelte
+++ frontend/src/ClassList.svelte
@@ -4,6 +4,8 @@
   import ClassDetail from './ClassDetail.svelte'
 
   let classes = null;
+  let selectedSubject = null;
+
   onMount(async () => {
     classes = await refetch();
   });
@@ -18,7 +20,11 @@
 {#if !classes}
   Loading...
 {:else}
-  {#each classes as clazz}
+  {#each [...new Set(classes.map(c => c.subject_abbr))] as subject}
+    <button class="btn btn-link p-0 pr-2" class:font-weight-bold={selectedSubject == subject} on:click={() => selectedSubject = subject}>{subject}</button>
+  {/each}
+
+  {#each classes.filter(c => c.subject_abbr == selectedSubject || selectedSubject == null) as clazz}
     <ClassDetail clazz={clazz} on:update={async () => classes = await refetch()} />
   {/each}
 {/if}
