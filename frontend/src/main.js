25d5997f7cc530c86b5912dbf3f6114932f6beeb
Daniel Trnka <daniel.trnka@gmail.com>
2020-11-04 17:29:11 +0100

    import only some languages from hljs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,29 @@
+import 'highlight.js/styles/github.css'
+import hljs from 'highlight.js/lib/core'
+import clike from 'highlight.js/lib/languages/c-like'
+import c from 'highlight.js/lib/languages/c'
+import cpp from 'highlight.js/lib/languages/cpp'
+import java from 'highlight.js/lib/languages/java'
+import python from 'highlight.js/lib/languages/python'
+import xml from 'highlight.js/lib/languages/xml'
+import bash from 'highlight.js/lib/languages/bash'
+import shell from 'highlight.js/lib/languages/shell'
+import makefile from 'highlight.js/lib/languages/makefile'
+
+hljs.registerLanguage('c-like', clike);
+hljs.registerLanguage('c', c); 
+hljs.registerLanguage('cpp', cpp); 
+hljs.registerLanguage('java', java); 
+hljs.registerLanguage('python', python); 
+hljs.registerLanguage('xml', xml); 
+hljs.registerLanguage('bash', bash); 
+hljs.registerLanguage('shell-session', shell);
+hljs.registerLanguage('makefile', makefile); 
+
+import App from './App.svelte'
+import TaskDetail from './TaskDetail.svelte'
+
+
 function createElement(name, component) {
 	customElements.define('kelvin-' + name, class extends HTMLElement {
 		connectedCallback() {
@@ -20,5 +46,5 @@ function createElement(name, component) {
 	});
 }
 
-createElement('app', require('./App.svelte'));
-createElement('submit-sources', require('./TaskDetail.svelte'));
+createElement('app', App);
+createElement('submit-sources', TaskDetail);
