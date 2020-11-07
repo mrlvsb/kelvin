690094fc2bad7b3ca5b2002907f4039dbef70e71
Daniel Trnka <daniel.trnka@gmail.com>
2020-11-07 22:30:05 +0100

    Summary comments with a bubble and code reuse

---
+++ frontend/src/SummaryComments.svelte
@@ -0,0 +1,43 @@
+<script>
+    import CommentForm from './CommentForm.svelte'
+    import Comment from './Comment.svelte'
+    import { createEventDispatcher } from 'svelte';
+
+    export let summaryComments;
+
+    let showForm = false;
+
+    const dispatch = createEventDispatcher();
+
+    function addComment(evt) {
+        dispatch('saveComment', {
+            text: evt.detail,
+            success: () => showForm = false,
+        });
+    }
+</script>
+
+<style>
+div :global(.CodeMirror) {
+    height: 100px;
+}
+
+button {
+    line-height: normal;
+    margin-top: -10px;
+}
+</style>
+
+{#each summaryComments as comment}
+    <Comment {...comment} on:saveComment />
+{/each}
+
+{#if showForm}
+    <div>
+        <CommentForm on:save={addComment} />
+    </div>
+{:else}
+    <button class="btn p-0" on:click={() => showForm = !showForm}>
+        <span class="iconify" data-icon="bx:bx-comment-add" data-inline="false" data-flip="vertical" data-height="20" title="Add new comment"></span>
+    </button>
+{/if}
