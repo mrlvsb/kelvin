d6c9f8163b70baf610b1e5e87b20720958cc27a9
Daniel Trnka <daniel.trnka@gmail.com>
2020-11-24 18:53:52 +0100

    Allow uploading files by dragging them to the window

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,10 @@
 export function fetch(url, options) {
   options = options || {};
   options.headers = options.headers || {};
-  options.headers['X-CSRFToken'] = document.querySelector('meta[name=csrf-token]').getAttribute('content');
+  options.headers['X-CSRFToken'] = csrfToken();
   return window.fetch(url, options);
+}
+
+export function csrfToken() {
+  return document.querySelector('meta[name=csrf-token]').getAttribute('content');
 }
