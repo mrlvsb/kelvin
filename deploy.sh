e3b41d7d494f5f492e975886ebd6555b5399bf6d
Daniel Trnka <daniel.trnka@gmail.com>
2021-02-08 19:19:53 +0100

    Deploy: restart all workers

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,13 @@ ssh kelvin@kelvin.cs.vsb.cz << COMMANDS
     npm run build
   )
   python manage.py collectstatic --no-input -c
-  sudo systemctl restart uwsgi kelvin-worker@0
+  sudo systemctl restart uwsgi "kelvin-worker@*"
   cd evaluator/images && ./build.sh
+  curl https://sentry.io/api/hooks/release/builtin/5459340/e234046479d3446064962c594a05b10e3acdb0ae6dd9985383e42773a7d79c0c/ \
+    -X POST \
+    -H 'Content-Type: application/json' \
+    -d '{"version": "'$(git rev-parse HEAD)'"}'
   echo OK
 COMMANDS
+
+
