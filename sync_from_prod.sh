8ae901179445396ac12fa3813f36ddc8a81a8f88
Daniel Trnka <daniel.trnka@gmail.com>
2021-04-18 14:23:21 +0200

    sync_from_db loads production data from pgsql

--- sync_from_prod.sh
+++ sync_from_prod.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-rsync -av kelvin@kelvin.cs.vsb.cz:kelvin/{db.sqlite3,submits,tasks} . 
-rsync -av kelvin@kelvin.cs.vsb.cz:kelvin/survey/surveys survey/
+docker rm -f kelvin_pgsql
+docker run -p 127.0.0.1:5432:5432 --name kelvin_pgsql -e POSTGRES_PASSWORD=pazzword -d postgres
+sleep 5
+ssh kelvin@kelvin.cs.vsb.cz pg_dump kelvin | pv | docker exec -i kelvin_pgsql psql -U postgres -d postgres
+rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/{tasks,exams} . 
+rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/submits/2020-S submits/ 
+rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/survey/surveys survey/
