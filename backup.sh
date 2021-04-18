a91f4ddb7e2130601f7a18cf1d43648055b25e11
Daniel Trnka <daniel.trnka@gmail.com>
2021-04-18 14:18:37 +0200

    Local backup creates dump from the pgsql

--- backup.sh
+++ backup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -ex
+
+semester=2020-S
+
 dst="backups/$(date +"%Y-%m-%d_%H:%M:%S")"
 mkdir -p "$dst"
 cd "$dst"
 
-rsync -avz kelvin@kelvin.cs.vsb.cz:kelvin/{db.sqlite3,submits,tasks} .
+ssh kelvin@kelvin.cs.vsb.cz 'pg_dump kelvin | xz' | pv > dump.sql.xz
+rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/tasks .
+mkdir submits
+rsync -avzP "kelvin@kelvin.cs.vsb.cz:kelvin/submits/$semester" submits/
+du -sh .
