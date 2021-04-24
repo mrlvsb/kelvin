47d2a7e97a88f3ccddfb61af443d259031582c2c
Jakub Beránek <berykubik@gmail.com>
2021-04-24 13:48:34 +0200

    Add more logging to MOSS

--- common/moss.py
+++ common/moss.py
@@ -4,6 +4,7 @@ import os
 import re
 import subprocess
 import tempfile
+from io import StringIO
 
 import django_rq
 import mosspy
@@ -14,8 +15,6 @@ from django.core.cache import caches
 from networkx.drawing.nx_agraph import write_dot
 
 from common.models import Submit
-
-logger = logging.getLogger('moss')
 
 MAX_FILE_SIZE = 128 * 1024
 
@@ -45,21 +44,28 @@ def check_file_size(path: str) -> bool:
     return 0 < os.path.getsize(path) <= MAX_FILE_SIZE
 
 
-def is_source_valid(source) -> bool:
-    return is_ext_allowed(source.virt) and check_file_size(source.phys)
+def is_source_valid(logger, source) -> bool:
+    if not is_ext_allowed(source.virt):
+        logger.warning(f"Skipping file {source.virt} because of extension")
+        return False
+    if not check_file_size(source.phys):
+        logger.warning(f"Skipping file {source.phys} because of file size")
+        return False
+    return True
 
 
-def add_file(moss: mosspy.Moss, submit: Submit, file_path: str, name: str, counters):
-    logger.info(
-        f"Task {submit.assignment.task_id}: adding student {submit.student.username} file {name} from {file_path}")
+def add_file(logger, moss: mosspy.Moss, file_path: str, name: str, counters):
+    logger.info(f"Adding file {name} from {file_path}")
     ext = name.split('.')[-1].lower()
     counters[ALLOWED_EXTENSIONS[ext]] += 1
     moss.addFile(file_path, name)
 
 
-def add_submit(moss: mosspy.Moss, submit: Submit, counters):
-    sources = [source for source in submit.all_sources() if is_source_valid(source)]
+def add_submit(logger, moss: mosspy.Moss, submit: Submit, counters):
+    logger.info(f"Checking submit {submit.id} by {submit.student.username}")
+    sources = [source for source in submit.all_sources() if is_source_valid(logger, source)]
     if not sources:
+        logger.warning("No files found")
         return
 
     filenames = set()
@@ -78,78 +84,104 @@ def add_submit(moss: mosspy.Moss, submit: Submit, counters):
         filepath = source.phys
         filename = generate_filename(source.virt)
         filenames.add(filename)
-        add_file(moss, submit, filepath, filename, counters)
+        add_file(logger, moss, filepath, filename, counters)
 
 
-@django_rq.job('default', timeout=60*15)
+@django_rq.job("default", timeout=60 * 15)
 def check_task(task_id):
-    logger.info(f"Task {task_id} plagiarism checking started")
+    log_stream = StringIO()
+    log_handler = logging.StreamHandler(log_stream)
+    log_handler.setFormatter(
+        logging.Formatter(f"Task {task_id}: %(asctime)s - %(levelname)s - %(message)s"))
+
+    logger = logging.getLogger(f"moss-{task_id}")
+    logger.addHandler(log_handler)
+    logger.setLevel(logging.INFO)
+
+    logger.info(f"Plagiarism checking started")
     counters = {lang: 0 for lang in ALLOWED_EXTENSIONS.values()}
 
-    submits = Submit.objects.filter(assignment__task__id=task_id).order_by('-submit_num')
-    m = mosspy.Moss(settings.MOSS_USERID)
-    # do not ignore matches that occur frequently
-    m.setIgnoreLimit(10000)
+    url = None
+    matches = []
+    success = True
 
-    # group submissions by directory
-    m.setDirectoryMode(1)
+    try:
+        submits = Submit.objects.filter(assignment__task__id=task_id).order_by("-submit_num")
+        m = mosspy.Moss(settings.MOSS_USERID)
+        # do not ignore matches that occur frequently
+        m.setIgnoreLimit(10000)
 
-    # template files
-    tpl_path = os.path.join(submits[0].assignment.task.dir(), "template")
-    for root, _, files in os.walk(tpl_path):
-        for f in files:
-            full_path = os.path.join(root, f)
-            if is_ext_allowed(f) and check_file_size(full_path):
-                logger.info(f"Task {task_id}: adding base file {f}")
-                m.addBaseFile(full_path)
+        # group submissions by directory
+        m.setDirectoryMode(1)
 
-    processed = set()
-    for submit in submits:
-        if submit.student_id not in processed:
-            try:
-                add_submit(m, submit, counters)
-                processed.add(submit.student_id)
-            except IOError:
-                logger.error(f"Cannot add submit {submit.id} to MOSS")
+        # template files
+        tpl_path = os.path.join(submits[0].assignment.task.dir(), "template")
+        for root, _, files in os.walk(tpl_path):
+            for f in files:
+                full_path = os.path.join(root, f)
+                if is_ext_allowed(f) and check_file_size(full_path):
+                    logger.info(f"Task {task_id}: adding base file {f}")
+                    m.addBaseFile(full_path)
+                else:
+                    logger.warning(f"Skipping template file {full_path}")
 
-    detected_lang = max(counters, key=counters.get)
-    if counters[detected_lang] > 0:
-        m.options['l'] = detected_lang
+        processed = set()
+        for submit in submits:
+            if submit.student_id not in processed:
+                try:
+                    add_submit(logger, m, submit, counters)
+                    processed.add(submit.student_id)
+                except IOError as e:
+                    logger.error(
+                        f"Cannot add submit {submit.id} by {submit.student.username} to MOSS: {e}")
 
-    logging.info(f"Sending these files to Moss: {m.files}")
-    url = m.send()
-    logging.info(f"Moss returned: {url}")
-    with tempfile.NamedTemporaryFile() as out:
-        m.saveWebPage(url, out.name)
+        detected_lang = max(counters, key=counters.get)
+        if counters[detected_lang] > 0:
+            m.options["l"] = detected_lang
 
-        matches = []
-        with open(out.name) as f:
-            regex = r'<TR>' \
-                '<TD><A HREF="(?P<link>[^"]+)">(?P<first_login>[^/]+)/(?P<first_path>.*?) \((?P<first_percent>\d+)%\)</A>' \
-                '\s*<TD><A HREF="[^"]+">(?P<second_login>[^/]+)/(?P<second_path>.*?) \((?P<second_percent>\d+)%\)</A>' \
-                '\s*<TD[^>]+>(?P<lines>\d+)'
+        logging.info(f"Sending files to Moss: {m.files}")
+        url = m.send()
+        logging.info(f"Moss returned: {url}")
+        with tempfile.NamedTemporaryFile() as out:
+            m.saveWebPage(url, out.name)
 
-            for m in re.finditer(regex, f.read()):
-                d = m.groupdict()
-                d['first_percent'] = int(d['first_percent'])
-                d['second_percent'] = int(d['second_percent'])
-                matches.append(d)
+            with open(out.name) as f:
+                regex = r'<TR>' \
+                        '<TD><A HREF="(?P<link>[^"]+)">(?P<first_login>[^/]+)/(?P<first_path>.*?) \((?P<first_percent>\d+)%\)</A>' \
+                        '\s*<TD><A HREF="[^"]+">(?P<second_login>[^/]+)/(?P<second_path>.*?) \((?P<second_percent>\d+)%\)</A>' \
+                        '\s*<TD[^>]+>(?P<lines>\d+)'
 
-            caches['default'].set(moss_result_cache_key(task_id), {
-                "url": url,
-                "matches": matches,
-                "timestamp": datetime.datetime.now()
-            }, timeout=60*60*24*90)
-    logger.info(f"Task {task_id} plagiarism checking finished, URL: {url}")
-    moss_delete_job_from_cache(task_id)
+                for m in re.finditer(regex, f.read()):
+                    d = m.groupdict()
+                    d["first_percent"] = int(d['first_percent'])
+                    d["second_percent"] = int(d['second_percent'])
+                    matches.append(d)
+        logger.info(f"Plagiarism checking finished, URL: {url}")
+    except BaseException as e:
+        logger.exception(e)
+        success = False
+    finally:
+        moss_delete_job_from_cache(task_id)
+
+    caches["default"].set(moss_result_cache_key(task_id), {
+        "success": success,
+        "url": url,
+        "matches": matches,
+        "timestamp": datetime.datetime.now(),
+        "log": log_stream.getvalue()
+    }, timeout=60 * 60 * 24 * 90)
 
 
 class MossResult:
-    def __init__(self, url, matches, opts, timestamp):
+    def __init__(self,
+                 success: bool, url: str, matches,
+                 opts, timestamp: datetime.datetime, log: str):
+        self.success = success
         self.url = url
         self.matches = matches
         self.opts = opts
         self.timestamp = timestamp
+        self.log = log
         self.G = nx.Graph()
 
         for match in matches:
@@ -166,7 +198,7 @@ class MossResult:
         for d in self.matches:
             percent = max(int(d['first_percent']), int(d['second_percent']))
             if percent > max_percent:
-               max_percent = percent
+                max_percent = percent
 
         with tempfile.NamedTemporaryFile("w") as out:
             G = self.G.copy()
@@ -200,6 +232,7 @@ class MossResult:
             graph = subprocess.check_output(["dot", "-T", "svg", out.name]).decode('utf-8')
             return re.sub(r'width="[^"]+" height="[^"]+"', '', graph)
 
+
 def moss_task_set_opts(task_id, opts):
     cache = caches['default']
     cache.set(f"moss.{task_id}.opts", opts, timeout=60*60*24*90)
@@ -215,6 +248,7 @@ def moss_task_get_opts(task_id):
         **opts,
         **caches['default'].get(f"moss.{task_id}.opts", {})
     }
+
 
 def moss_result(task_id, percent=None, lines=None):
     cache = caches['default']
@@ -232,12 +266,22 @@ def moss_result(task_id, percent=None, lines=None):
 
     matches = []
     for match in cache_entry["matches"]:
-        if min(match['first_percent'], match['second_percent']) >= opts['percent'] and int(match['lines']) >= opts['lines']:
-            match['first_fullname'] = User.objects.get(username=match['first_login']).get_full_name()
-            match['second_fullname'] = User.objects.get(username=match['second_login']).get_full_name()
+        if min(match['first_percent'], match['second_percent']) >= opts['percent'] and int(
+            match['lines']) >= opts['lines']:
+            match['first_fullname'] = User.objects.get(
+                username=match['first_login']).get_full_name()
+            match['second_fullname'] = User.objects.get(
+                username=match['second_login']).get_full_name()
             matches.append(match)
 
-    return MossResult(cache_entry.get("url"), matches, opts, cache_entry.get("timestamp"))
+    return MossResult(
+        cache_entry.get("success"),
+        cache_entry.get("url"),
+        matches,
+        opts,
+        cache_entry.get("timestamp"),
+        cache_entry.get("log")
+    )
 
 
 def moss_result_cache_key(task_id: int) -> str:
