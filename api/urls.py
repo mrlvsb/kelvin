74e024efb5c170d37c79f68e090ea2bb0800a4c8
Jan Gaura <jan.gaura@gmail.com>
2023-06-09 09:01:01 +0200

    API: List all subjects stored in Kelvin

--- api/urls.py
+++ api/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('classes/all', views.all_classes),
     path('classes/<int:class_id>/add_students', views.add_student_to_class),
     path('subject/<subject_abbr>', views.subject_list),
+    path('subjects/all', views.subjects_all),
     path('reevaluate_task/<int:task_id>', views.reevaluate_task),
     path('search', views.search),
     path('transfer_students', views.transfer_students),
