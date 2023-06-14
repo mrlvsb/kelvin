c835ac4645bf769913e8f880d3bc91cfc85f5b9f
Jan Gaura <jan.gaura@gmail.com>
2023-06-14 15:42:14 +0200

    INBUS config + INBUS list of subject versions

--- common/inbus/auth.py
+++ common/inbus/auth.py
@@ -4,14 +4,13 @@ from django.conf import settings
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
-
-INBUS_TOKEN_URL = 'https://inbus.vsb.cz/oauth/token'
+from . import config
 
 
 def authenticate(client_id=settings.INBUS_CLIENT_ID, client_secret=settings.INBUS_CLIENT_SECRET) -> Dict:
     client = BackendApplicationClient(client_id=client_id)
     oauth = OAuth2Session(client=client)
-    token = oauth.fetch_token(token_url=INBUS_TOKEN_URL, client_id=client_id, client_secret=client_secret)
+    token = oauth.fetch_token(token_url=config.INBUS_TOKEN_URL, client_id=client_id, client_secret=client_secret)
 
     return token
 
---
+++ common/inbus/config.py
@@ -0,0 +1,7 @@
+import urllib.parse
+
+INBUS_BASE_URL: str = 'https://inbus.vsb.cz/service/'
+INBUS_SERVICE_EDISON_URL: str = urllib.parse.urljoin(INBUS_BASE_URL, 'edison/v1/')
+INBUS_SERVICE_IDM_URL: str = urllib.parse.urljoin(INBUS_BASE_URL, 'idm/v1/')
+
+INBUS_TOKEN_URL: str = 'https://inbus.vsb.cz/oauth/token'
