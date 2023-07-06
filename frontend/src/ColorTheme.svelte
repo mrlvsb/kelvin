9d837c1e7ee6f4f5745bbc4bb0cb86e41fcb16ec
rtaborsky02@gmail.com <rtaborsky02@gmail.com>
2023-07-06 23:19:14 +0200

    hotfix: theme auto icon not selecting and showing

--- frontend/src/ColorTheme.svelte
+++ frontend/src/ColorTheme.svelte
@@ -3,6 +3,7 @@
   import 'bootstrap/js/dist/button';
   import { curTheme } from "./utils";
 
+  let workingTheme = "auto";
   const htmlData = document.querySelector("html").dataset;
   let autoInUse = false;
   const icons = {
@@ -19,18 +20,19 @@
   }
 
   function selectTheme(event) {
-    const theme = event.srcElement.dataset.theme;
-    if (theme == "auto") {
-      localStorage.removeItem("color-theme"); 
+    workingTheme = event.srcElement.dataset.theme;
+    if (workingTheme == "auto") {
+      localStorage.removeItem("color-theme");
+      autoInUse = true;
       detectTheme();
       window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", detectTheme);
       return;
     }
     // theme is manually selected
-    curTheme.set(theme);
+    curTheme.set(workingTheme);
     window.matchMedia("(prefers-color-scheme: light)").removeEventListener("change", detectTheme);
-    localStorage.setItem("color-theme", $curTheme);
-    htmlData.bsTheme = $curTheme;
+    localStorage.setItem("color-theme", workingTheme);
+    htmlData.bsTheme = workingTheme;
   }
 
 
@@ -38,25 +40,25 @@
     detectTheme();
     window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", detectTheme);
   } else {
-    htmlData.bsTheme = localStorage.getItem("color-theme");
+    workingTheme = htmlData.bsTheme = localStorage.getItem("color-theme");
     curTheme.set(htmlData.bsTheme);
   }
 </script>
 
 <li class="nav-item dropdown">
   <button class="btn btn-link nav-link dropdown-toggle" href="#" type="button" data-bs-toggle="dropdown" aria-expanded="false" title="Change theme">
-    <i class="bi {icons[$curTheme] === undefined ? "bi-circle-half" : icons[$curTheme]}"></i>
+    <i class="bi {icons[workingTheme] === undefined ? "bi-circle-half" : icons[workingTheme]}"></i>
     <span class="d-md-none ms-1">Change theme</span>
   </button>
   <ul class="dropdown-menu dropdown-menu-end shadow">
-    <li><button on:click={selectTheme} class="d-flex dropdown-item" class:active={$curTheme == "auto"} data-theme="auto">
-      <i class="me-1 bi bi-circle-half"></i>Auto<i class="opacity-75 ms-auto bi bi-check2" hidden={$curTheme != "auto"}></i>
+    <li><button on:click={selectTheme} class="d-flex dropdown-item" class:active={workingTheme == "auto"} data-theme="auto">
+      <i class="me-1 bi bi-circle-half"></i>Auto<i class="opacity-75 ms-auto bi bi-check2" hidden={workingTheme != "auto"}></i>
     </button></li>
-    <li><button on:click={selectTheme} class="d-flex dropdown-item" class:active={$curTheme == "light"} data-theme="light">
-      <i class="me-1 bi bi-sun"></i>Light mode<i class="opacity-75 ms-auto bi bi-check2" hidden={$curTheme != "light"}></i>
+    <li><button on:click={selectTheme} class="d-flex dropdown-item" class:active={workingTheme == "light"} data-theme="light">
+      <i class="me-1 bi bi-sun"></i>Light mode<i class="opacity-75 ms-auto bi bi-check2" hidden={workingTheme != "light"}></i>
     </button></li>
-    <li><button on:click={selectTheme} class="d-flex dropdown-item" class:active={$curTheme == "dark"} data-theme="dark">
-      <i class="me-1 bi bi-moon-stars"></i>Dark mode<i class="opacity-75 ms-auto bi bi-check2" hidden={$curTheme != "dark"}></i>
+    <li><button on:click={selectTheme} class="d-flex dropdown-item" class:active={workingTheme == "dark"} data-theme="dark">
+      <i class="me-1 bi bi-moon-stars"></i>Dark mode<i class="opacity-75 ms-auto bi bi-check2" hidden={workingTheme != "dark"}></i>
     </button></li>
   </ul>
 </li>
