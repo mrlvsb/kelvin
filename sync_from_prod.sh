2fd2b075830fabafe25199cf1088e8123ec2f83b
Jan Gaura <jan.gaura@gmail.com>
2023-10-04 13:26:33 +0200

    Sync script updated to current semester

--- sync_from_prod.sh
+++ sync_from_prod.sh
@@ -3,5 +3,5 @@
 set -ex
 ssh kelvin@kelvin.cs.vsb.cz pg_dump --clean kelvin | docker exec -i kelvin_db psql -U "$DB_USERNAME" -d "$DB_DATABASE"
 rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/tasks . 
-rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/submits/2022-W submits/ 
+rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/submits/2023-W submits/
 rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/survey/surveys survey/
