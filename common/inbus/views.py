d8ec7fb6430cf22d5c95a9a5a3d81245ecfd5198
Jakub Beránek <berykubik@gmail.com>
2024-09-05 14:24:03 +0200

    Migrate more endpoints from `HttpResponse` to `JsonResponse`

--- common/inbus/views.py
+++ common/inbus/views.py
@@ -2,7 +2,7 @@ import serde.json
 
 from django.core.cache import cache
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 from . import inbus
 from common.inbus import dto
@@ -16,24 +16,21 @@ def subject_versions(request):
         subject_versions = inbus.subject_versions()
         cache.set("subject_versions", subject_versions, 60 * 60)
 
-    # TODO: When upgrading, see: https://docs.djangoproject.com/en/4.2/ref/request-response/#httpresponse-objects
-    # for a way to set content type.
-    return HttpResponse(serde.json.to_json(subject_versions), content_type="application/json")
+    # safe=False is used because we return a list directly
+    return JsonResponse(serde.to_dict(subject_versions), safe=False)
 
 
 @user_passes_test(is_teacher)
 def schedule_subject_by_version_id(request, subject_version_id: dto.SubjectVersionId):
     schedule_subject = inbus.schedule_subject_by_version_id(subject_version_id)
 
-    # TODO: When upgrading, see: https://docs.djangoproject.com/en/4.2/ref/request-response/#httpresponse-objects
-    # for a way to set content type.
-    return HttpResponse(serde.json.to_json(schedule_subject), content_type="application/json")
+    # safe=False is used because we return a list directly
+    return JsonResponse(serde.to_dict(schedule_subject), safe=False)
 
 
 @user_passes_test(is_teacher)
 def students_in_concrete_activity(request, concrete_activity_id: dto.ConcreteActivityId):
     study_relations = inbus.students_in_concrete_activity(concrete_activity_id)
 
-    # TODO: When upgrading, see: https://docs.djangoproject.com/en/4.2/ref/request-response/#httpresponse-objects
-    # for a way to set content type.
-    return HttpResponse(serde.json.to_json(study_relations), content_type="application/json")
+    # safe=False is used because we return a list directly
+    return JsonResponse(serde.to_dict(study_relations), safe=False)
