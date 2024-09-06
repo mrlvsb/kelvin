9e148effa6e24c8be8fcbb6d8cccb4416612b86e
Jakub Beránek <berykubik@gmail.com>
2024-09-06 09:22:12 +0200

    Update to Django 4.2

--- kelvin/settings.py
+++ kelvin/settings.py
@@ -141,7 +141,15 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+    },
+}
 
 INTERNAL_IPS = ("127.0.0.1",)
 
