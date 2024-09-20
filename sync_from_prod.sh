b96c0b2635d2cdfb7601ae6f08062e704142156b
JersyJ <dlichnovsky1@gmail.com>
2024-09-20 02:42:48 +0200

    Add services

--- sync_from_prod.sh
+++ sync_from_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . .env
 set -ex
-ssh kelvin@kelvin.cs.vsb.cz pg_dump --clean kelvin | docker exec -i kelvin_db psql -U "$DB_USERNAME" -d "$DB_DATABASE"
+ssh kelvin@kelvin.cs.vsb.cz pg_dump --clean kelvin | docker exec -i kelvin_db psql -U "$DATABASE__USERNAME" -d "$DATABASE__DB"
 rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/tasks . 
 rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/submits/2023-W submits/
 rsync -avzP kelvin@kelvin.cs.vsb.cz:kelvin/survey/surveys survey/
