26a1e0e7a5c90d51256d60983a76c47ada6e8f10
Jakub Beránek <berykubik@gmail.com>
2024-09-28 10:52:35 +0200

    Revert "Reformat code"
    This reverts commit 3000cae0fd7b76d03ae5936eb9d33f0287fac5ca.

--- frontend/src/markdown.js
+++ frontend/src/markdown.js
@@ -1,28 +1,33 @@
-import { marked } from 'marked';
+import { marked } from 'marked'
 import DOMPurify from 'dompurify';
-import hljs from 'highlight.js/lib/core';
+import hljs from 'highlight.js/lib/core'
 
 marked.setOptions({
-    highlight: function (code, lang) {
-        if (lang) {
-            try {
-                return hljs.highlight(lang, code).value;
-            } catch (err) {
-                if (typeof Sentry !== 'undefined') {
-                    Sentry.captureException(err);
-                }
-            }
+  highlight: function(code, lang) {
+    if(lang) {
+      try {
+        return hljs.highlight(lang, code).value;
+      } catch (err) {
+        if(typeof(Sentry) !== 'undefined') {
+          Sentry.captureException(err);
         }
-        return hljs.highlightAuto(code).value;
-    },
-    breaks: true
+      }
+    }
+    return hljs.highlightAuto(code).value;
+  },
+  breaks: true,
 });
 
 const sanitizeOpts = {
-    ALLOWED_TAGS: ['img', 'p', 'b', 'strong', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'i', 'ul', 'ol', 'li', 'pre', 'code', 'a', 'br', 'span'],
-    ALLOWED_ATTR: ['href', 'src', 'class']
+  ALLOWED_TAGS: [
+    'img', 'p', 'b', 'strong', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'i', 'ul', 'ol', 'li', 'pre', 'code', 'a', 'br', 'span'
+  ],
+  ALLOWED_ATTR: [
+    'href', 'src', 'class',
+  ]
 };
 
 export function safeMarkdown(text, opts = null) {
     return DOMPurify.sanitize(marked(text, opts), sanitizeOpts);
 }
+
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,44 +1,46 @@
-import { writable } from 'svelte/store';
+import {writable} from 'svelte/store'
 
 export function clickOutside(node) {
-    const handleClick = (event) => {
-        if (node && !node.contains(event.target) && !event.defaultPrevented) {
-            node.dispatchEvent(new CustomEvent('click_outside', node));
-        }
-    };
+  const handleClick = event => {
+    if (node && !node.contains(event.target) && !event.defaultPrevented) {
+      node.dispatchEvent(
+        new CustomEvent('click_outside', node)
+      )
+    }
+  }
 
-    document.addEventListener('click', handleClick, true);
+  document.addEventListener('click', handleClick, true);
 
-    return {
-        destroy() {
-            document.removeEventListener('click', handleClick, true);
-        }
-    };
+  return {
+    destroy() {
+      document.removeEventListener('click', handleClick, true);
+    }
+  }
 }
 
 let localStorageStores = {};
 export function localStorageStore(key, initialValue) {
-    if (!(key in localStorageStores)) {
-        const saved = localStorage.getItem(key);
-        if (saved) {
-            try {
-                initialValue = JSON.parse(saved);
-            } catch (exception) {
-                console.log(`Failed to load ${key}: ${exception}`);
-            }
+  if(!(key in localStorageStores)) {
+      const saved = localStorage.getItem(key);
+      if(saved) {
+        try {
+          initialValue = JSON.parse(saved);
+        } catch(exception) {
+          console.log(`Failed to load ${key}: ${exception}`);
         }
+      }
 
-        const { subscribe, set } = writable(initialValue);
+      const {subscribe, set} = writable(initialValue);
 
-        localStorageStores[key] = {
-            subscribe,
-            set(value) {
-                localStorage.setItem(key, JSON.stringify(value));
-                set(value);
-            }
-        };
-    }
-    return localStorageStores[key];
+      localStorageStores[key] = {
+        subscribe,
+        set(value) {
+          localStorage.setItem(key, JSON.stringify(value));
+          set(value);
+        },
+      }
+  }
+  return localStorageStores[key];
 }
 
-export const curTheme = writable('');
+export const curTheme = writable("");
