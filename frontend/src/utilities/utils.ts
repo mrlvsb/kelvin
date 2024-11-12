fc0ddf44c9c270582a0b4d1c3229bbbbf30e8911
Patrik Mintěl <ja@patrick115.eu>
2024-11-12 19:09:55 +0100

    Added function over getFromAPI, which fills csrfToken

--- frontend/src/utilities/utils.ts
+++ frontend/src/utilities/utils.ts
@@ -1,3 +1,5 @@
+import { csrfToken } from '../api';
+
 type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
 /**
@@ -7,17 +9,20 @@ type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
  * @param data data which will be sent to the API
  * @param method HTTP method, if you want to override the default GET/POST
  * @note If data is passed, the request will be a POST request, otherwise a GET request, if not overridden by method parameter
+ * @param headers Headers for the request
  *
  * @returns $ReturnType, if the request was successful, otherwise undefined
  */
 export const getFromAPI = async <$ReturnType>(
     url: string,
+    method?: Method,
     data?: unknown,
-    method?: Method
+    headers?: HeadersInit
 ): Promise<$ReturnType | undefined> => {
     try {
         const response = await fetch(url, {
             method: method || (data ? 'POST' : 'GET'),
+            headers,
             body: data ? JSON.stringify(data) : undefined
         });
 
@@ -31,6 +36,29 @@ export const getFromAPI = async <$ReturnType>(
         console.error(error);
         return undefined;
     }
+};
+
+/**
+ * Get data from endpoint with {@link getFromAPI()}, but with already filled header `X-CSRFToken`.
+ *
+ * @param url url to fetch
+ * @param data data which will be sent to the API
+ * @param method HTTP method, if you want to override the default GET/POST
+ * @note If data is passed, the request will be a POST request, otherwise a GET request, if not overridden by method parameter
+ * @param headers Headers for the request
+ *
+ * @returns $ReturnType, if the request was successful, otherwise undefined
+ */
+export const getDataWithCSRF = async <$ReturnType>(
+    url: string,
+    method?: Method,
+    data?: unknown,
+    headers?: HeadersInit
+): Promise<$ReturnType | undefined> => {
+    const CSRF = {
+        'X-CSRFToken': csrfToken()
+    };
+    return getFromAPI(url, method, data, headers ? { ...headers, ...CSRF } : CSRF);
 };
 
 /**
