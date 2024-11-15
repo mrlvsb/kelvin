6279e26d982f8fe7e1b70ed6580f30bec2071d4b
Patrik Mintěl <ja@patrick115.eu>
2024-11-15 11:27:49 +0100

    Added info about component

--- frontend/src/components/Notifications.vue
+++ frontend/src/components/Notifications.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+/**
+ * This components displays bell with number of notifications next to it.
+ * After opening, it shows list of all notifications, which user got.
+ * It is available to both teachers and students, and it is accesible from 
+ * It is only available to teachers, and it is accessible from main layout on each page,
+ * next to name.
+ */
+
 import {
   notifications,
   pushNotifications,
