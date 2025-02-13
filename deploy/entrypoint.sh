625579cacf27be003e76f902076e6586d8b041e5
Jakub Beránek <berykubik@gmail.com>
2025-02-13 23:33:56 +0100

    Collect static files in Docker entrypoint

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,4 +4,8 @@ set -eux
 
 python manage.py migrate
 
+# We need to run collection of static items after the container is started,
+# because the static directory might be a Docker volume, and we need to overwrite it.
+python manage.py collectstatic --no-input --clear
+
 exec uwsgi --ini uwsgi.ini
