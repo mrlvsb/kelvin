dfa6afeecc262a86475125c8b60498b291644ecd
Jan Gaura <jan.gaura@gmail.com>
2025-11-04 21:53:18 +0100

    Exclude dolos results from sync script + semester update

--- sync_from_prod.sh
+++ sync_from_prod.sh
@@ -2,6 +2,6 @@
 . .env
 set -ex
 ssh kelvin@kelvin.cs.vsb.cz docker exec kelvin_db pg_dump -U "$DATABASE__USERNAME" --clean kelvin | docker exec -i kelvin_db psql -U "$DATABASE__USERNAME" -d "$DATABASE__DB"
-rsync -avzP kelvin@kelvin.cs.vsb.cz:/srv/kelvin/kelvin/tasks .
-rsync -avzP kelvin@kelvin.cs.vsb.cz:/srv/kelvin/kelvin/submits/2024-S submits/
+rsync -avzP --exclude='*/.dolos-results/' kelvin@kelvin.cs.vsb.cz:/srv/kelvin/kelvin/tasks .
+rsync -avzP kelvin@kelvin.cs.vsb.cz:/srv/kelvin/kelvin/submits/2025-W submits/
 rsync -avzP kelvin@kelvin.cs.vsb.cz:/srv/kelvin/kelvin/survey/surveys survey/
