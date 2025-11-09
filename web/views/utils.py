3df304ba0e40115d37141f8df12bd1a348864d31
Firestone82 <mikpav@email.cz>
2025-11-09 12:24:58 -0600

    Removed unused function

--- web/views/utils.py
+++ web/views/utils.py
@@ -1,12 +1,9 @@
-from django.conf import settings
 from django.core import signing
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
-from django.shortcuts import redirect
 from unidecode import unidecode
 
 from common.models import Submit
-from common.utils import is_teacher
 
 
 def file_response(file, filename: str, mimetype: str) -> HttpResponse:
@@ -22,20 +19,3 @@ def authenticate_submit_token_request(request, submit: Submit) -> None:
             return None
 
     raise PermissionDenied()
-
-
-def authenticate_submit_user_request(request, submit: Submit, allow_token=False) -> None:
-    if "token" in request.GET and allow_token:
-        token = signing.loads(request.GET["token"], max_age=3600)
-        if token.get("submit_id") == submit.id:
-            return None
-        else:
-            raise PermissionDenied()
-
-    if not request.user.is_authenticated:
-        return redirect(f"{settings.LOGIN_URL}?next={request.path}")
-
-    if not is_teacher(request.user) and request.user.username != submit.student.username:
-        raise PermissionDenied()
-
-    return None
