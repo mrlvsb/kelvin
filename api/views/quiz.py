9a6b1e65e3e4d29672539b02045b66e8b4e5ffc8
Jan Gaura <jan.gaura@gmail.com>
2025-11-22 16:48:37 +0100

    Refactor a few JSON error responses in Quizes

--- api/views/quiz.py
+++ api/views/quiz.py
@@ -14,10 +14,7 @@ from unidecode import unidecode
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
-from django.http import (
-    HttpRequest,
-    HttpResponseForbidden,
-)
+from django.http import HttpRequest
 from django.urls import reverse
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, resolve_url
@@ -110,11 +107,11 @@ def quiz_results(request: HttpRequest, enrolled_id: int, is_submit: int):
 
     enrolled_quiz = get_object_or_404(EnrolledQuiz, pk=enrolled_id)
 
-    if request.user.id != enrolled_quiz.student.id:
-        return HttpResponseForbidden()
+    if request.user.pk != enrolled_quiz.student.pk:
+        return JsonResponse({"error": "Forbidden"}, status=403)
 
     if enrolled_quiz.submitted:
-        return HttpResponseForbidden()
+        return JsonResponse({"error": "Quiz already submitted"}, status=403)
 
     submit_answers_dto = from_json(SubmitAnswersDto, request.body.decode("utf-8"))
 
@@ -147,8 +144,8 @@ def quiz_scoring(request: HttpRequest, enrolled_id: int):
 
     if enrolled_quiz.scored_by is None:
         enrolled_quiz.scored_by = teacher
-    elif enrolled_quiz.scored_by.id != teacher.id:
-        return HttpResponseForbidden()
+    elif enrolled_quiz.scored_by.pk != teacher.pk:
+        return JsonResponse({"error": "Forbidden"}, status=403)
 
     enrolled_quiz.scoring = json.loads(to_json(scoring_dto.scoring))
 
