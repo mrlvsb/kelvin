cd9fdf1684ad7f45ec075fd498deaa4ec9f56dca
Jan Stránský <janstr06@gmail.com>
2025-11-26 22:56:38 +0100

    use noqa instead of specific except

--- evaluator/images/cargo/entry.py
+++ evaluator/images/cargo/entry.py
@@ -205,7 +205,7 @@ finally:
     for file in SANITIZED_FILES:
         try:
             os.unlink(file)
-        except FileNotFoundError:
+        except: # noqa
             pass
 
 with open("result.html", "w") as out:
