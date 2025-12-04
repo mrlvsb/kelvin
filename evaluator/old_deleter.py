56fb2a936a038392edae4009573ebaf183684702
Jan Stránský <janstr06@gmail.com>
2025-12-04 17:52:55 +0100

    move to docker API in deleter

--- evaluator/old_deleter.py
+++ evaluator/old_deleter.py
@@ -1,22 +1,13 @@
-import json
-import subprocess
+import docker
 from datetime import datetime, timezone
 
 
-def create_kill_command(container_id):
-    return ["docker", "kill", container_id]
-
-
-def delete_old_containers(n):
-    data = subprocess.check_output(["docker", "ps", "--format", "json"]).decode().split("\n")[:-1]
-    for container in data:
-        container_data = json.loads(container)
-        print(container_data)
-        created_at = datetime.strptime(container_data["CreatedAt"], "%Y-%m-%d %H:%M:%S %z %Z")
-        print(created_at)
-        now = datetime.now(timezone.utc)
+def delete_old_containers(n, socket_path="unix://var/run/docker.sock"):
+    client = docker.APIClient(base_url=socket_path)
+    containers = client.containers()
+    for container in containers:
+        created_at = container["Created"]
+        now = datetime.now(timezone.utc).timestamp()
         elapsed = now - created_at
-        print(elapsed.total_seconds())
-        if elapsed.total_seconds() > n:
-            cmd = create_kill_command(container_data["ID"])
-            subprocess.run(cmd)
+        if elapsed > n:
+            client.kill(container["Id"])
