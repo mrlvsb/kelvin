4c394f7eebfe3554c4ffbf41237abcc48389008e
Jan Stránský <janstr06@gmail.com>
2025-12-05 16:52:17 +0100

    split to function

--- task_creator/apps.py
+++ task_creator/apps.py
@@ -5,24 +5,7 @@ class TaskCreatorConfig(AppConfig):
     name = "task_creator"
 
     def ready(self):
-        # models must be imported after they are loaded
-        from scheduler.models.scheduled_task import CronTask, BaseTask
-        from scheduler.models.args import TaskArg
-        from django.contrib.contenttypes.models import ContentType
-        existing_job = CronTask.objects.filter(name="deleter").first()
-        if existing_job:
-            existing_job.delete()
-        deleter_job = CronTask.objects.create(
-            name="deleter",
-            callable="evaluator.old_deleter.delete_old_containers",
-            enabled=True,
-            queue="evaluator",
-            cron_string="0 3 * * *",
-                )
-        content_type = ContentType.objects.get_for_model(model)
-        TaskArg.objects.create(
-            object_id=deleter_job.id,
-            content_type=content_type,
-            arg_type="int",
-            val="1800",
-            )
+        # due to how django loads models, we can't import this at the top of the file
+        from task_creator.cron_creator import create_crontask
+        create_crontask("deleter", "evaluator.old_deleter.delete_old_containers", "0 * * * *", args=[("int", "1800")])
+
---
+++ task_creator/cron_creator.py
@@ -0,0 +1,24 @@
+from scheduler.models.scheduled_task import CronTask, BaseTask
+from scheduler.models.args import TaskArg
+from django.contrib.contenttypes.models import ContentType
+
+
+def create_crontask(name, callable, cron_string, args=[], enabled=True, queue="evaluator"):
+    existing_job = CronTask.objects.filter(name=name).first()
+    if existing_job:
+        existing_job.delete()
+    deleter_job = CronTask.objects.create(
+        name=name,
+        callable=callable,
+        enabled=enabled,
+        queue=queue,
+        cron_string=cron_string,
+            )
+    content_type = ContentType.objects.get_for_model(CronTask)
+    for arg in args:
+        TaskArg.objects.create(
+            object_id=deleter_job.id,
+            content_type=content_type,
+            arg_type=arg[0],
+            val=arg[1],
+            )
