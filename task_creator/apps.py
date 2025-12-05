531a70848273351c9c033f9e5a907b3eaee4a5c1
Jan Stránský <janstr06@gmail.com>
2025-12-05 17:11:20 +0100

    use try - db does not have to run with makemigrations

--- task_creator/apps.py
+++ task_creator/apps.py
@@ -8,9 +8,13 @@ class TaskCreatorConfig(AppConfig):
         # due to how django loads models, we can't import this at the top of the file
         from task_creator.cron_creator import create_crontask
 
-        create_crontask(
-            "deleter",
-            "evaluator.old_deleter.delete_old_containers",
-            "0 * * * *",
-            args=[("int", "1800")],
-        )
+        try:
+            create_crontask(
+                "deleter",
+                "evaluator.old_deleter.delete_old_containers",
+                "0 * * * *",
+                args=[("int", "1800")],
+            )
+        # Database is not running, skip creating the cron task
+        except Exception:
+            pass
