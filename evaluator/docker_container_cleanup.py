421a87ea1c14a3f29d8337e94b01a44ff65df611
Jan Stránský <janstr06@gmail.com>
2025-12-06 15:05:12 +0100

    add explanation comment to docker_container_cleanup

--- evaluator/docker_container_cleanup.py
+++ evaluator/docker_container_cleanup.py
@@ -2,12 +2,13 @@ import docker
 from datetime import datetime, timezone
 
 
-def delete_old_containers(n, socket_path="unix://var/run/docker.sock"):
+# Deletes Docker containers that have been running for more than max_lifetime seconds.
+def delete_old_containers(max_lifetime: int, socket_path="unix://var/run/docker.sock"):
     client = docker.APIClient(base_url=socket_path)
     containers = client.containers()
     for container in containers:
         created_at = container["Created"]
         now = datetime.now(timezone.utc).timestamp()
         elapsed = now - created_at
-        if elapsed > n:
+        if elapsed > max_lifetime:
             client.kill(container["Id"])
