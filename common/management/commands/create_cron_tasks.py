a5efadec4a7739d4d30b7e8e56d254c13d05f56d
Jan Stránský <janstr06@gmail.com>
2025-12-07 08:56:26 +0100

    move from ready to dedicated command

---
+++ common/management/commands/create_cron_tasks.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from common.cron_creator import create_crontask
+import logging
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **opts):
+        try:
+            # We want to delete containers older than 30 minutes every hour
+            create_crontask(
+                "deleter",
+                "evaluator.docker_container_cleanup.delete_old_containers",
+                cron_string="0 * * * *",
+                args=[("int", "1800")],
+            )
+        # Database is not running, skip creating the cron task
+        except Exception as e:
+            logging.warning(f"Could not create task: {e}")
