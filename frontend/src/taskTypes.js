939c2b534064466da72341241d2754a5ecd3c689
Daniel Krásný <53856821+DanielKrasny@users.noreply.github.com>
2025-12-20 20:41:11 +0100

    feat: ClassDetail filter for Quizzes

--- frontend/src/taskTypes.js
+++ frontend/src/taskTypes.js
@@ -4,5 +4,6 @@ export const task_types = [
     { key: 'exam', value: 'Exam' },
     { key: 'project', value: 'Project' },
     { key: 'laboratory', value: 'Laboratory' },
+    { key: 'quiz', value: 'Quiz' },
     { key: 'other', value: 'Other' }
 ];
