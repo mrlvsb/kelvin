97f7f53816c2a2cd42d000289d41671297e6d1f2
Firestone82 <mikpav@email.cz>
2026-01-17 13:15:07 -0600

    Add rating to SuggestedComment

--- frontend/src/components/submit/SuggestedComment.vue
+++ frontend/src/components/submit/SuggestedComment.vue
@@ -1,29 +1,21 @@
 <script lang="ts" setup>
 import { computed, ref } from 'vue';
 import CommentForm from './CommentForm.vue';
+import StarRating from '../StarRating.vue';
 import { safeMarkdown } from '../../markdown.js';
 import { user } from '../../global.js';
 import { hideComments, HideCommentsState } from '../../stores';
 import { useSvelteStore } from '../../utilities/useSvelteStore';
 import Toast from 'bootstrap/js/dist/toast';
+import { Comment } from '../../types/TaskDetail';
 
 const props = withDefaults(
-  defineProps<{
-    id?: number | null;
-    author?: string;
-    text?: string;
-    meta?: { review: { id: number } } | null;
-    files?: unknown[] | null;
-    rating?: number;
-    summary?: boolean;
-  }>(),
+  defineProps<
+    Comment & {
+      summary?: boolean;
+    }
+  >(),
   {
-    id: null,
-    author: '',
-    text: '',
-    meta: null,
-    files: null,
-    rating: 0,
     summary: false
   }
 );
@@ -31,9 +23,10 @@ const props = withDefaults(
 const emit = defineEmits(['resolveSuggestion']);
 
 const toast = ref(null);
+const processed = ref(false);
 const editing = ref(false);
 const sending = ref(false);
-const processed = ref(false);
+const committedRating = ref(props.meta.review.rating ?? 0);
 
 const currentUser = useSvelteStore(user, null);
 const currentHideComments = useSvelteStore(hideComments, HideCommentsState.NONE);
@@ -160,71 +153,122 @@ const handleSave = async (text: string) => {
   sending.value = false;
   editing.value = false;
 };
+
+const handleRating = async (rating: number) => {
+  sending.value = true;
+  const suggestionId = props.meta.review.id;
+
+  const previousRating = committedRating.value;
+  committedRating.value = rating;
+
+  /* TODO: Use emits instead of implementing API calls directly in the component, if URL would change in Django,
+      this method would break. -> need to pass url as a prop.
+  */
+  const res = await fetch(`/api/v2/llm/suggestions/${suggestionId}/rate`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      commentId: suggestionId,
+      rating
+    })
+  });
+
+  const json = await res.json();
+
+  if (!res.ok) {
+    showToast(json.detail, 'warning');
+    committedRating.value = previousRating;
+  }
+
+  sending.value = false;
+};
 </script>
 
 <template>
-  <div class="position-fixed top-0 end-0 mt-5 me-3">
-    <div
-      ref="toast"
-      class="toast align-items-center text-white bg-success border-0"
-      role="alert"
-      aria-live="assertive"
-      aria-atomic="true"
-    >
-      <div class="d-flex">
-        <div class="toast-body">Unexpected error occurred. Please try again later.</div>
+  <div class="suggested-comment" v-bind="$attrs">
+    <div class="position-fixed top-0 end-0 mt-5 me-3">
+      <div
+        ref="toast"
+        class="toast align-items-center text-white bg-success border-0"
+        role="alert"
+        aria-live="assertive"
+        aria-atomic="true"
+      >
+        <div class="d-flex">
+          <div class="toast-body">Unexpected error occurred. Please try again later.</div>
 
-        <button
-          type="button"
-          class="btn-close btn-close-dark me-2 m-auto"
-          data-bs-dismiss="toast"
-          aria-label="Close"
-        />
+          <button
+            type="button"
+            class="btn-close btn-close-dark me-2 m-auto"
+            data-bs-dismiss="toast"
+            aria-label="Close"
+          />
+        </div>
       </div>
     </div>
-  </div>
 
-  <template v-if="showComment">
-    <template v-if="currentUser?.teacher && !processed">
-      <div class="comment ai-review">
-        <div class="comment-header">
-          <strong>{{ author }}</strong>
+    <template v-if="showComment">
+      <template v-if="currentUser?.teacher && !processed">
+        <div class="comment ai-review">
+          <div class="comment-header">
+            <strong>{{ author }}</strong>
 
-          <div v-if="currentUser?.teacher && !editing" class="comment-actions">
-            <button
-              v-if="summary === true"
-              title="Dismiss"
-              class="icon-button"
-              @click.prevent="handleReject"
-            >
-              <span class="iconify" data-icon="cil-x"></span>
-            </button>
-
-            <template v-else>
+            <div v-if="currentUser?.teacher && !editing" class="comment-actions">
               <button
-                title="Turn into a student-visible comment"
+                v-if="summary === true"
+                title="Dismiss"
                 class="icon-button"
-                @click.prevent="handleAccept"
+                :disabled="sending"
+                @click.prevent="handleReject"
               >
-                <span class="iconify" data-icon="cil-check"></span>
-              </button>
-
-              <button title="Edit" class="icon-button" @click.prevent="handleEdit">
-                <span class="iconify" data-icon="cil-pencil"></span>
-              </button>
-
-              <button title="Reject" class="icon-button" @click.prevent="handleReject">
                 <span class="iconify" data-icon="cil-x"></span>
               </button>
-            </template>
-          </div>
-        </div>
 
-        <div v-if="!editing" class="comment-text" v-html="safeMarkdown(text)"></div>
-        <CommentForm v-else :comment="text" :disabled="sending" @save="handleSave" />
-      </div>
+              <template v-else>
+                <button
+                  title="Turn into a student-visible comment"
+                  class="icon-button"
+                  :disabled="sending"
+                  @click.prevent="handleAccept"
+                >
+                  <span class="iconify" data-icon="cil-check"></span>
+                </button>
+
+                <button
+                  title="Edit"
+                  class="icon-button"
+                  :disabled="sending"
+                  @click.prevent="handleEdit"
+                >
+                  <span class="iconify" data-icon="cil-pencil"></span>
+                </button>
+
+                <button
+                  title="Reject"
+                  class="icon-button"
+                  :disabled="sending"
+                  @click.prevent="handleReject"
+                >
+                  <span class="iconify" data-icon="cil-x"></span>
+                </button>
+              </template>
+
+              <StarRating
+                :committed-rating="committedRating"
+                :disabled="sending"
+                @rate="handleRating"
+              />
+            </div>
+          </div>
+
+          <div v-if="!editing" class="comment-text" v-html="safeMarkdown(text)"></div>
+          <CommentForm v-else :comment="text" :disabled="sending" @save="handleSave" />
+        </div>
+      </template>
     </template>
-  </template>
+  </div>
 </template>
 
 <style scoped>
@@ -236,6 +280,7 @@ const handleSave = async (text: string) => {
 
 .comment-actions {
   display: flex;
+  align-items: center;
   gap: 0.4rem;
 }
 
